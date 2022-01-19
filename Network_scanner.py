585f458729d9d0d7194c11da796c60668a94fb2d
NewToKali <85852629+NewToKali@users.noreply.github.com>
2022-01-18 23:42:07 -0500

    Create Network_scanner.py
    Intended for Debian Linux. This is a network scanner that allows you to discover all devices in the subnet by sending an arp message with the broadcast mac address to all hosts in the subnet.
    
    Prerequisites:
    
    scapy.all
    optparse
    To install: 
    
    pip install scapy.all 
    pip install optparse
    
    Usage:
    --help  ---> will show available options
    example: 
    python Network_scanner.py -t <subnet like 10.0.0.1/24>
    
    Note:
    by default, this program will use the original NIC (eth0). If you intend to use this with an external NIC (wlan0) please turn off eth0.

---
+++ Network_scanner.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import scapy.all as scapy
+import optparse
+ 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="ip", help="chose the ip which u want to find")
+    # return parser.parse_args()
+    (options, arg) = parser.parse_args()
+    if not options.ip:
+        parser.error("please specify an interface type help for more info")
+ 
+    return options
+def scan(ip):
+ 
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    #answerd_list , unanswerd_lits = scapy.srp(arp_request_broadcast, timeout= 1)
+    answerd_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False )[0]
+    # print(answerd_list.summary())
+    # print("IP\t\t\t\t MAC ADDRESS\n----------------------------------------------------")
+    listOfScan = []
+    for packet in answerd_list :
+        # print(packet[1].show())
+        dictionary = {"ip": packet[1].psrc, "mac": packet[1].hwsrc}
+        # print(packet[1].psrc +"\t\t\t" +packet[1].hwsrc)
+        listOfScan.append(dictionary)
+ 
+    # print(listOfScan)
+    return listOfScan
+    # arp_request_broadcast.show()
+    # print(arp_request.summary())
+    # scapy.arping(ip)
+    # scapy.ls(scapy.ARP())
+def print_results(list_scan):
+    print("IP\t\t\t\t MAC ADDRESS\n----------------------------------------------------")
+    for client in list_scan:
+        print(client["ip"]+"\t\t\t"+client["mac"])
+ 
+gotip = get_arguments()
+scan_resuts = scan(gotip.ip)
+print_results(scan_resuts)
+
